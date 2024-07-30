e567e3f406cd2e545c54e3a77b0e879034235a70
Sven Augustin <sven.augustin@psi.ch>
2024-07-30 14:26:35 +0200

    tried to group bad masked regions a bit better

--- dap/algos/addmask.py
+++ dap/algos/addmask.py
@@ -3,55 +3,50 @@
 def calc_apply_additional_mask(detector, pixel_mask_pf):
     if detector == "JF06T08V04":
         # edge pixels
-        pixel_mask_pf[67:1097,1063] = 0
         pixel_mask_pf[0:1030, 1100] = 0
-
-        pixel_mask_pf[1106:2136, 1131] = 0
-        pixel_mask_pf[1039:2069, 1168] = 0
-
-        pixel_mask_pf[1039:2069, 1718] = 0
-        pixel_mask_pf[1039:2069, 1681] = 0
-
-        pixel_mask_pf[1106:2136, 618] = 0
-
-        pixel_mask_pf[1106:2136, 581] = 0
-
-        pixel_mask_pf[67:1097,513] = 0
-
-        pixel_mask_pf[67:1097, 550] = 0
-
+        pixel_mask_pf[0:1030, 1613] = 0
         pixel_mask_pf[0:1030, 1650] = 0
 
-        pixel_mask_pf[0:1030, 1613] = 0
+        pixel_mask_pf[67, 0:1063] = 0
 
-        pixel_mask_pf[1106, 68:582] = 0
-
-        pixel_mask_pf[1096, 550:1064] = 0
-        pixel_mask_pf[1106, 618:1132] = 0
+        pixel_mask_pf[67:1097, 513] = 0
+        pixel_mask_pf[67:1097, 550] = 0
+        pixel_mask_pf[67:1097, 1063] = 0
 
         pixel_mask_pf[1029, 1100:1614] = 0
-        pixel_mask_pf[1039, 1168:1682] = 0
+        pixel_mask_pf[1029, 1650:2163] = 0
 
+        pixel_mask_pf[1039, 1168:1682] = 0
         pixel_mask_pf[1039, 1718:2230] = 0
 
-        pixel_mask_pf[1096, 0:513] = 0
+        pixel_mask_pf[1039:2069, 1168] = 0
+        pixel_mask_pf[1039:2069, 1681] = 0
+        pixel_mask_pf[1039:2069, 1718] = 0
 
-        pixel_mask_pf[1029, 1650:2163] = 0
+        pixel_mask_pf[1096, 0:513] = 0
+        pixel_mask_pf[1096, 550:1064] = 0
+
+        pixel_mask_pf[1106, 68:582] = 0
+        pixel_mask_pf[1106, 618:1132] = 0
+
+        pixel_mask_pf[1106:2136, 581] = 0
+        pixel_mask_pf[1106:2136, 618] = 0
+        pixel_mask_pf[1106:2136, 1131] = 0
 
         pixel_mask_pf[2068, 1168:2232] = 0
 
-        pixel_mask_pf[67,0:1063] = 0
-
-        #bad region in left bottom inner module
+        # first bad region in left bottom inner module
         pixel_mask_pf[842:1097, 669:671] = 0
 
-        #second bad region in left bottom inner module
+        # second bad region in left bottom inner module
         pixel_mask_pf[1094, 620:807] = 0
 
         # vertical line in upper left bottom module
         pixel_mask_pf[842:1072, 87:90] = 0
 
+        # horizontal line?
         pixel_mask_pf[1794, 1503:1550] = 0
+
 
     if detector == "JF17T16V01":
         # mask module 11
