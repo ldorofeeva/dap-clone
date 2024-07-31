abeab715ddeb0080c56d7f70ce99ca966b2452f4
Sven Augustin <sven.augustin@psi.ch>
2024-07-31 11:12:10 +0200

    consistent naming

--- dap/algos/roi.py
+++ dap/algos/roi.py
@@ -20,8 +20,8 @@ def calc_roi(results, data, pixel_mask_pf, threshold_value_choice):
     if not (len(roi_x1) == len(roi_x2) == len(roi_y1) == len(roi_y2)):
         return
 
-    roi_results = []
-    roi_results_normalised = []
+    roi_intensities = []
+    roi_intensities_normalised = []
 
     roi_intensities_x = []
     roi_intensities_proj_x = []
@@ -30,7 +30,7 @@ def calc_roi(results, data, pixel_mask_pf, threshold_value_choice):
         data_roi = data[iy1:iy2, ix1:ix2]
 
         roi_sum = np.nansum(data_roi)
-        roi_results.append(roi_sum)
+        roi_intensities.append(roi_sum)
 
         if threshold_value_choice == "NaN":
             roi_area = (y2 - y1) * (x2 - x1)
@@ -38,7 +38,7 @@ def calc_roi(results, data, pixel_mask_pf, threshold_value_choice):
         else:
             roi_sum_norm = np.nanmean(data_roi)
 
-        roi_results_normalised.append(roi_sum_norm)
+        roi_intensities_normalised.append(roi_sum_norm)
 
         roi_intensity_x = [x1, x2]
         roi_intensity_proj_x = np.nansum(data_roi, axis=0).tolist()
@@ -46,8 +46,8 @@ def calc_roi(results, data, pixel_mask_pf, threshold_value_choice):
         roi_intensities_x.append(roi_intensity_x)
         roi_intensities_proj_x.append(roi_intensity_proj_x)
 
-    results["roi_intensities"] = [float(r) for r in roi_results]
-    results["roi_intensities_normalised"] = [float(r) for r in roi_results_normalised]
+    results["roi_intensities"] = [float(r) for r in roi_intensities]
+    results["roi_intensities_normalised"] = [float(r) for r in roi_intensities_normalised]
 
     results["roi_intensities_x"] = roi_intensities_x
     results["roi_intensities_proj_x"] = roi_intensities_proj_x
