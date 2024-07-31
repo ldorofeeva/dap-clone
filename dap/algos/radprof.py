851a186538620479dd9e664c50043f440f61fb8b
Sven Augustin <sven.augustin@psi.ch>
2024-07-31 17:50:40 +0200

    readability

--- dap/algos/radprof.py
+++ dap/algos/radprof.py
@@ -6,13 +6,18 @@ def calc_radial_integration(results, data, keep_pixels, pixel_mask_pf, center_ra
 
     if keep_pixels is None and pixel_mask_pf is not None:
         keep_pixels = (pixel_mask_pf != 0)
+
     if center_radial_integration is None:
-        center_radial_integration = [results["beam_center_x"], results["beam_center_y"]]
+        center_radial_integration = [
+            results["beam_center_x"],
+            results["beam_center_y"]
+        ]
         r_radial_integration = None
+
     if r_radial_integration is None:
         r_radial_integration, nr_radial_integration = prepare_radial_profile(data, center_radial_integration, keep_pixels)
-        r_min_max = [int(np.min(r_radial_integration)), int(np.max(r_radial_integration)) + 1]
-
+        r_min = int(np.min(r_radial_integration))
+        r_max = int(np.max(r_radial_integration)) + 1
 
     apply_threshold = results.get("apply_threshold", False)
 
@@ -32,16 +37,16 @@ def calc_radial_integration(results, data, keep_pixels, pixel_mask_pf, center_ra
         silent_region_min is not None and
         silent_region_max is not None and
         silent_region_max > silent_region_min and
-        silent_region_min > r_min_max[0] and
-        silent_region_max < r_min_max[1]
+        silent_region_min > r_min and
+        silent_region_max < r_max
     ):
-
-        integral_silent_region = np.sum(rp[silent_region_min:silent_region_max])
+        silent_region = rp[silent_region_min:silent_region_max]
+        integral_silent_region = np.sum(silent_region)
         rp = rp / integral_silent_region
         results["radint_normalised"] = [silent_region_min, silent_region_max]
 
-    results["radint_I"] = list(rp[r_min_max[0]:])
-    results["radint_q"] = r_min_max
+    results["radint_I"] = rp[r_min:].tolist()
+    results["radint_q"] = [r_min, r_max]
 
     return keep_pixels, center_radial_integration, r_radial_integration
 
