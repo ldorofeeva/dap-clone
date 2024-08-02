dd7eda4834a217d4e7e12a960665c8544a9b1f20
Sven Augustin <sven.augustin@psi.ch>
2024-08-02 15:11:13 +0200

    renamed variable pfdata to data

--- dap/algos/mask.py
+++ dap/algos/mask.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def calc_mask_pixels(pfdata, pixel_mask_pf):
+def calc_mask_pixels(data, pixel_mask_pf):
     if pixel_mask_pf is None:
         return
 
-    pfdata[pixel_mask_pf != 1] = np.nan #TODO: boolean mask
+    data[pixel_mask_pf != 1] = np.nan #TODO: boolean mask
 
 
 
