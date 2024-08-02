062cc6055cc718c441a7cce03d2197882aa6b17e
Sven Augustin <sven.augustin@psi.ch>
2024-08-02 15:10:52 +0200

    use the same validation logic as in other cases

--- dap/algos/mask.py
+++ dap/algos/mask.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 def calc_mask_pixels(pfdata, pixel_mask_pf):
-    if pixel_mask_pf is not None:
-        pfdata[pixel_mask_pf != 1] = np.nan #TODO: boolean mask
+    if pixel_mask_pf is None:
+        return
+
+    pfdata[pixel_mask_pf != 1] = np.nan #TODO: boolean mask
 
 
 
