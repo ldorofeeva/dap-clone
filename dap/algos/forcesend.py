93f0702c4178cb1ebe7580027644b013e43fe011
Sven Augustin <sven.augustin@psi.ch>
2024-08-08 16:09:36 +0200

    early exit 2

--- dap/algos/forcesend.py
+++ dap/algos/forcesend.py
@@ -14,33 +14,33 @@ def calc_force_send(results, data, pixel_mask_pf, image, n_aggregated_images, da
         data_summed = None
         n_aggregated_images = 1
 
-    if apply_threshold or apply_aggregation:
-        if apply_threshold and all(k in results for k in ("threshold_min", "threshold_max")):
-            threshold_min = float(results["threshold_min"])
-            threshold_max = float(results["threshold_max"])
-            data[data < threshold_min] = 0.0
-            if threshold_max > threshold_min:
-                data[data > threshold_max] = 0.0
-
-        if apply_aggregation and "aggregation_max" in results:
-            if data_summed is not None:
-                data += data_summed
-                n_aggregated_images += 1
-            data_summed = data.copy()
-            data_summed[data == -np.nan] = -np.nan #TODO: this does nothing
-            results["aggregated_images"] = n_aggregated_images
-            results["worker"] = 1 #TODO: keep this for backwards compatibility?
-
-            if n_aggregated_images >= results["aggregation_max"]:
-                force_send_visualisation = True
-                data_summed = None
-                n_aggregated_images = 1
-
-        if pixel_mask_pf is not None:
-            data[~pixel_mask_pf] = np.nan
-
-    else:
+    if not apply_threshold and not apply_aggregation:
         data = image
+        return data, force_send_visualisation, n_aggregated_images, data_summed
+
+    if apply_threshold and all(k in results for k in ("threshold_min", "threshold_max")):
+        threshold_min = float(results["threshold_min"])
+        threshold_max = float(results["threshold_max"])
+        data[data < threshold_min] = 0.0
+        if threshold_max > threshold_min:
+            data[data > threshold_max] = 0.0
+
+    if apply_aggregation and "aggregation_max" in results:
+        if data_summed is not None:
+            data += data_summed
+            n_aggregated_images += 1
+        data_summed = data.copy()
+        data_summed[data == -np.nan] = -np.nan #TODO: this does nothing
+        results["aggregated_images"] = n_aggregated_images
+        results["worker"] = 1 #TODO: keep this for backwards compatibility?
+
+        if n_aggregated_images >= results["aggregation_max"]:
+            force_send_visualisation = True
+            data_summed = None
+            n_aggregated_images = 1
+
+    if pixel_mask_pf is not None:
+        data[~pixel_mask_pf] = np.nan
 
     return data, force_send_visualisation, n_aggregated_images, data_summed
 
